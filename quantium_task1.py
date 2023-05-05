3a0b7187d821924219abc2f421f292ad6ed1ffb5
Joyce Wang <114795700+joycewv@users.noreply.github.com>
2023-05-05 09:17:44 +0000

    combine pink morsel data csv

--- quantium_task1.py
+++ quantium_task1.py
@@ -4,23 +4,35 @@ import pandas as pd
 
 #Task 2
 
-# read data from daily_sales_data files
-
+# read and extract pink morsel data from daily_sales_data files
+#data0
 df0 = pd.read_csv('data/daily_sales_data_0.csv')
 data0 = df0[df0['product'] == 'pink morsel']
-#print(data0)
+data0.to_csv('data/pink_morsel_data0.csv', index=False)
 
+#data1
 df1 = pd.read_csv('data/daily_sales_data_1.csv')
 data1 = df1[df1['product'] == 'pink morsel']
-#print(data1)
+data1.to_csv('data/pink_morsel_data1.csv', index=False)
 
+#data2
 df2 = pd.read_csv('data/daily_sales_data_2.csv')
 data2 = df2[df2['product'] == 'pink morsel']
-#print(data2)
+data2.to_csv('data/pink_morsel_data2.csv', index=False)
 
+#combine 3 pink morsel data files in 1
+pink0 = pd.read_csv('data/pink_morsel_data0.csv')
+pink1 = pd.read_csv('data/pink_morsel_data1.csv')
+pink2 = pd.read_csv('data/pink_morsel_data2.csv')
+pink = [pink0, pink1, pink2]
+dfp = pd.concat(pink)
+dfp.to_csv('data/pink_morsel_all.csv', index=False)
 
+#Calculate Sales Data
+df = pd.read_csv('data/pink_morsel_all.csv')
+dfs = df.assign(sales=lambda x: x.price.split('$') * x.quantity)
+print(dfs)
+#dfs = df.assign(sales=)
 
-
-
-#df = pd.read_csv('daily_sales_data_1.csv')
-#df = pd.read_csv('daily_sales_data_2.csv')
+#def SalesData():
+    #data0['sales'] =
