984a4faccae7cded0915493cbb806ab2b4d2d126
Joyce Wang <114795700+joycewv@users.noreply.github.com>
2023-05-05 23:24:01 +0000

    completed task 2

--- quantium_task1.py
+++ quantium_task1.py
@@ -8,31 +8,43 @@ import pandas as pd
 #data0
 df0 = pd.read_csv('data/daily_sales_data_0.csv')
 data0 = df0[df0['product'] == 'pink morsel']
-data0.to_csv('data/pink_morsel_data0.csv', index=False)
+data0.to_csv('data/data_processing/pink_morsel_data0.csv', index=False)
 
 #data1
 df1 = pd.read_csv('data/daily_sales_data_1.csv')
 data1 = df1[df1['product'] == 'pink morsel']
-data1.to_csv('data/pink_morsel_data1.csv', index=False)
+data1.to_csv('data/data_processing/pink_morsel_data1.csv', index=False)
 
 #data2
 df2 = pd.read_csv('data/daily_sales_data_2.csv')
 data2 = df2[df2['product'] == 'pink morsel']
-data2.to_csv('data/pink_morsel_data2.csv', index=False)
+data2.to_csv('data/data_processing/pink_morsel_data2.csv', index=False)
 
 #combine 3 pink morsel data files in 1
-pink0 = pd.read_csv('data/pink_morsel_data0.csv')
-pink1 = pd.read_csv('data/pink_morsel_data1.csv')
-pink2 = pd.read_csv('data/pink_morsel_data2.csv')
+pink0 = pd.read_csv('data/data_processing/pink_morsel_data0.csv')
+pink1 = pd.read_csv('data/data_processing/pink_morsel_data1.csv')
+pink2 = pd.read_csv('data/data_processing/pink_morsel_data2.csv')
 pink = [pink0, pink1, pink2]
 dfp = pd.concat(pink)
-dfp.to_csv('data/pink_morsel_all.csv', index=False)
+dfp.to_csv('data/data_processing/pink_morsel_all.csv', index=False)
 
-#Calculate Sales Data
-df = pd.read_csv('data/pink_morsel_all.csv')
-dfs = df.assign(sales=lambda x: x.price.split('$') * x.quantity)
-print(dfs)
-#dfs = df.assign(sales=)
+#Assigning easier to remember name
+df = pd.read_csv('data/data_processing/pink_morsel_all.csv')
 
-#def SalesData():
-    #data0['sales'] =
+#Remove the $ sign from price column
+df['price'] = df['price'].replace({'\$':''}, regex=True)
+df.to_csv('data/data_processing/no-dollar-sign.csv', index=False)
+
+#Ensure column price and quantity are numbers
+df['price'] = df['price'].astype("float")
+df['quantity'] = df['quantity'].astype("float")
+
+#Create a new column for total sales
+df['sales'] = df['price']*df['quantity']
+df.to_csv('data/data_processing/total_sales_ps.csv', index=False)
+
+#deleting product, price and quantity columns
+df = df.drop(['product', 'price', 'quantity'], axis=1)
+df.to_csv('data/total_sales_ps_dropped.csv', index=False)
+
+
